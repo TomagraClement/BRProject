526e5c9e4316460126c596a9405ba9bdc9d57e27
Clement <t.clement@hotmail.be>
2020-04-15 22:42:08 +0200

    index cut in header footer

---
+++ html/index.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<!-- Va chercher le code dans header.php et l'ajoute ici-->
+    <?php include('templates/header.php'); ?>
+<!-- Pareil pour le footer-->
+    <?php include('templates/footer.php'); ?>
+</html>
+
+</body>
+</html>
---
+++ html/templates/header.php
@@ -0,0 +1,7 @@
+<head>
+    <title>Bike Rental</title>
+    <link rel="stylesheet" href="../../css/style.css"/>
+</head>
+
+<body>
+Header
